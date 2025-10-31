399a9b76c267f908cd62bbe6c012577db63e9b9d
sydney-mtvgn <sydney.montevirgen2027@gmail.com>
2025-10-31 10:26:07 -0400

    lesson

--- basics.py
+++ basics.py
@@ -1,54 +1,62 @@
-# Defining a function
-def function_name():
-  pass
-# Calling a function
-function_name()
+# # Defining a function
+# def function_name():
+#   pass
+# # Calling a function
+# function_name()
 
-def print_header():
-  print("="*20)
-  print("Student Report")
-  print("="*20)
+# def print_header():
+#   print("="*20)
+#   print("Student Report")
+#   print("="*20)
 
-print_header()
+# print_header()
 
-# Empty Line
-print("\n")
+# # Empty Line
+# print("\n")
 
-def draw_square():
-# Draws a 5x5 square using asterisks
-# print("*****")
-# print("*****")
-# print("*****")
-# print("*****")
-# print("*****")
+# def draw_square():
+# # Draws a 5x5 square using asterisks
+# # print("*****")
+# # print("*****")
+# # print("*****")
+# # print("*****")
+# # print("*****")
 
-  for i in range(5):
-   print("*****")
+#   for i in range(5):
+#    print("*****")
 
-def morning_routine():
-  print("Brush teeth and change")
-  print("Pack bag and make bed")
-  print("Drive to school")
-pass
+# def morning_routine():
+#   print("Brush teeth and change")
+#   print("Pack bag and make bed")
+#   print("Drive to school")
+# pass
 
-def school_schedule():
-  print("History, English, Business")
-  print("Compsci, Lunch, Bio")
-  print("Gym, Stats")
-pass
+# def school_schedule():
+#   print("History, English, Business")
+#   print("Compsci, Lunch, Bio")
+#   print("Gym, Stats")
+# pass
 
-def after_school():
-  print("Volleyball")
-  print("Homework")
-  print("Shower and eat")
-pass
+# def after_school():
+#   print("Volleyball")
+#   print("Homework")
+#   print("Shower and eat")
+# pass
 
-def daily_routine():
-  morning_routine()
-  print()
-  school_schedule()
-  print()
-  after_school()
+# def daily_routine():
+#   morning_routine()
+#   print()
+#   school_schedule()
+#   print()
+#   after_school()
 
-daily_routine()
+# daily_routine()
+
+def cal_area_rect():
+    w = 5
+    l = 10
+    area = w * l
+    print(area)
+
+cal_area_rect()
 
